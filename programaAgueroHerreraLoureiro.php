3ddd3093bc3d63b0a7531ddb7b21bafbb50e6b3d
guillermoagueronqn <guillermoagueronqn@gmail.com>
2022-11-05 12:24:27 -0300

    Se agregan datos de integrantes, se completa la funcion 1 y se agrega la funcion 2

--- programaAgueroHerreraLoureiro.php
+++ programaAgueroHerreraLoureiro.php
@@ -5,15 +5,18 @@ include_once("wordix.php");
 /***** DATOS DE LOS INTEGRANTES *******/
 /**************************************/
 
-/* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
-/* ... COMPLETAR ... */
-
-
+/*
+| Apellido, Nombre                | Legajo   | Carrera | Mail                                  | GitHub             |
+| Agüero Mendez, Guillermo Andres | FAI-3844 | TUDW    | guillermo.aguero@est.fi.uncoma.edu.ar | guillermoagueronqn |
+| Herrera, Julio Federico         | FAI-4285 | TUDW    | julio.herrera@est.fi.uncoma.edu.ar    | ELHACHESALTA       |
+| Loureiro, Jazmin Loureiro       | FAI-4228 | TUDW    | jazmin.loureiro@est.fi.uncoma.edu.ar  | Jazmin-Loureiro    | 
+*/
 
 /**************************************/
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
 
+///////////////////////////// FUNCION 1 /////////////////////////////////////////
 /**
  * Obtiene una colección de palabras
  * @return array
@@ -23,14 +26,33 @@ function cargarColeccionPalabras()
     $coleccionPalabras = [
         "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
-        /* ... COMPLETAR ... Agregar 5 palabras más */
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "PERRO", "OVNIS", "RUGBY", "BUCEO", "BUSCO"
     ];
-
     return ($coleccionPalabras);
 }
 
-/* ... COMPLETAR ... */
+///////////////////////////// FUNCION 2 /////////////////////////////////////////
+/**
+ * Obtiene una colección de partidas
+ * @return array
+ */
+function cargarPartidas(){
+    // array $coleccionPartidas
+    $coleccionPartidas = [];
+    $coleccionPartidas[0]=["palabraWordix"=>"QUESO", "jugador"=>"majo", "intentos"=>6, "puntaje"=>10];
+    $coleccionPartidas[1]=["palabraWordix"=>"CASAS", "jugador"=>"julio", "intentos"=>3, "puntaje"=>14];
+    $coleccionPartidas[2]=["palabraWordix"=>"MUJER", "jugador"=>"rudolf", "intentos"=>4, "puntaje"=>13];
+    $coleccionPartidas[3]=["palabraWordix"=>"YUYOS", "jugador"=>"jazmin", "intentos"=>6, "puntaje"=>12];
+    $coleccionPartidas[4]=["palabraWordix"=>"NAVES", "jugador"=>"julio", "intentos"=>4, "puntaje"=>13];
+    $coleccionPartidas[5]=["palabraWordix"=>"PISOS", "jugador"=>"majo", "intentos"=>3, "puntaje"=>14];
+    $coleccionPartidas[6]=["palabraWordix"=>"FUEGO", "jugador"=>"andres", "intentos"=>6, "puntaje"=>0];
+    $coleccionPartidas[7]=["palabraWordix"=>"RASGO", "jugador"=>"jazmin", "intentos"=>1, "puntaje"=>16];
+    $coleccionPartidas[8]=["palabraWordix"=>"GATOS", "jugador"=>"majo", "intentos"=>2, "puntaje"=>15];
+    $coleccionPartidas[9]=["palabraWordix"=>"GOTAS", "jugador"=>"rudolf", "intentos"=>6, "puntaje"=>11];
+    return $coleccionPartidas;
+}
+
 
 
 
